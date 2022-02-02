7252dd0d78a51670720255b13ca6329d4d6d2798
victorlouisdg <victorlouisdg@gmail.com>
2022-02-02 13:14:45 +0100

    expose camera module

--- airo_blender_toolkit/__init__.py
+++ airo_blender_toolkit/__init__.py
@@ -1,4 +1,5 @@
+import airo_blender_toolkit.camera as camera
 from airo_blender_toolkit.hdri import download_hdri, load_hdri
 
 # Prevents F401 unused imports
-__all__ = ("download_hdri", "load_hdri")
+__all__ = ("download_hdri", "load_hdri", "camera")
