fa3ff6d935e6f7eb1adf718c6d93a31989075bee
victorlouisdg <victorlouisdg@gmail.com>
2022-02-04 10:07:35 +0100

    random_texture_name function

--- airo_blender_toolkit/__init__.py
+++ airo_blender_toolkit/__init__.py
@@ -2,7 +2,17 @@ import airo_blender_toolkit.camera as camera
 from airo_blender_toolkit.geometry import rotate_vertex
 from airo_blender_toolkit.hdri import download_hdri, load_hdri
 from airo_blender_toolkit.object import make_object
+from airo_blender_toolkit.texture import random_texture_name
 from airo_blender_toolkit.visible_vertices import is_visible, visible_vertices
 
 # Prevents F401 unused imports
-__all__ = ("download_hdri", "load_hdri", "camera", "rotate_vertex", "make_object", "is_visible", "visible_vertices")
+__all__ = (
+    "download_hdri",
+    "load_hdri",
+    "camera",
+    "rotate_vertex",
+    "make_object",
+    "is_visible",
+    "visible_vertices",
+    "random_texture_name",
+)
